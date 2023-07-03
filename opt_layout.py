badd7dea8730f8e9b688ee122a147ecae0d7bf81
MatanPazi <matanpazi@gmail.com>
2023-07-03 13:41:35 +0300

    Added more patterns to test.

--- opt_layout.py
+++ opt_layout.py
@@ -5,9 +5,22 @@ import glob
 # Pattern_Layer = 2
 # pdf_name = 'PS_LuluCardi_Pattern(COPYSHOP_24x36)_BCUP.pdf'
 
+# Direction_Layer = 0
+# Pattern_Layer = 3
+# pdf_name = 'LL Leo Pattern Size A0.pdf'
+
+# Direction_Layer = 0
+# Pattern_Layer = 3
+# pdf_name = 'LL Leo Pattern Size A0.pdf'
+
 Direction_Layer = 0
-Pattern_Layer = 3
-pdf_name = 'LL Leo Pattern Size A0.pdf'
+Pattern_Layer = 1
+pdf_name = 'bt67-A0-pattern.pdf'
+
+## Seems like all the data is on layer 0...???
+# Direction_Layer = 0
+# Pattern_Layer = 1
+# pdf_name = 'BLOMMA TANK-A0-copyshop.pdf'
 
 pdf_out = 'Out_{num}.pdf'
 img_out = 'Out_{num}.png'
@@ -18,7 +31,7 @@ desired_layers = [Direction_Layer,Pattern_Layer]
 pdfLayers(pdf_name, pdf_out, desired_layers)
 size = pdf2image(desired_layers, pdf_out, img_out)
 pattern_contours = find_pattern_contours(img_out.format(num=Pattern_Layer), False)
-potential_dir_contours, potential_contour_pattern, pattern_contours = find_potential_direction_contours(img_out.format(num=Direction_Layer), pattern_contours)
+potential_dir_contours, potential_contour_pattern, pattern_contours = find_potential_direction_contours(img_out.format(num=Pattern_Layer), pattern_contours)
 copies, lining, main_fabric, fold, dir_cnt = find_text(img_out.format(num=Direction_Layer), pattern_contours, potential_dir_contours, potential_contour_pattern)
 rotation_angles = save_patterns(img_out.format(num=Pattern_Layer), pattern_contours, dir_cnt, potential_contour_pattern, ptrn_imgs)
 fold_patterns(fold, ptrn_imgs, rotation_angles, size)
