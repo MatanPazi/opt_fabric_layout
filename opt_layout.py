557106f2925c3cdd4f5c336ce5a6745af600d029
MatanPazi <matanpazi@gmail.com>
2023-07-04 13:27:07 +0300

    Added more patterns to test.

--- opt_layout.py
+++ opt_layout.py
@@ -9,18 +9,28 @@ import glob
 # Pattern_Layer = 3
 # pdf_name = 'LL Leo Pattern Size A0.pdf'
 
-Direction_Layer = 0
-Pattern_Layer = 1
-pdf_name = 'bt119-A0-pattern.pdf'
-
 # Direction_Layer = 0
 # Pattern_Layer = 1
-# pdf_name = 'bt67-A0-pattern.pdf'
+# pdf_name = 'bt119-A0-pattern.pdf'
+
+Direction_Layer = 0
+Pattern_Layer = 1
+pdf_name = 'bt67-A0-pattern.pdf'
 
 ## Seems like all the data is on layer 0...???
 # Direction_Layer = 0
 # Pattern_Layer = 1
 # pdf_name = 'BLOMMA TANK-A0-copyshop.pdf'
+
+# There's a legend for "cut on fold" so doesn't say so on the arrow itself. Need to add legend layer to read.
+# Direction_Layer = 8
+# Pattern_Layer = 9
+# pdf_name = '9-BAS_trapeze_patronAVECmarges-AtelierCharlotteAuzou_A0_34-48.pdf'
+
+# Made up of more than 1 page, need to handle differently...
+# Direction_Layer = 1
+# Pattern_Layer = 2
+# pdf_name = 'PS_ByrdieButtonup_UniversalPatternPieces(A0).pdf'
 
 pdf_out = 'Out_{num}.pdf'
 img_out = 'Out_{num}.png'
